f21349f7828e8e3330ad940f085b1a115c2b4b23
YM-13 <exitospec1.com>
2023-06-13 23:31:40 -0600

    13.06.2023

--- main.py
+++ main.py
@@ -1,1 +1,77 @@
-print("The firs Telegram bot)
+# import requests
+# import time
+#POLLING
+#
+# API_URL: str = 'https://api.telegram.org/bot'
+# API_CATS_URL: str = 'https://api.thecatapi.com/v1/images/search'
+#
+# BOT_TOKEN: str = '6212096846:AAEW3aKYpvclR8dydmVNYCsjeVRNcuJJMvU'
+# TEXT: str = 'Привет! Лови котика!'
+# ERROR_TEXT: str = 'Здесь должна была быть картинка с котиком :('
+#
+# offset: int = -5
+# counter: int = 0
+# cat_response: requests.Response
+# cat_link: str
+# chat_id: int
+#
+#
+# while counter < 100:
+#     print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+#     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+#
+#     if updates['result']:
+#         for result in updates['result']:
+#             offset = result['update_id']
+#             chat_id = result['message']['from']['id']
+#             cat_response = requests.get(API_CATS_URL)
+#             if cat_response.status_code == 200:
+#                 cat_link = cat_response.json()[0]["url"]
+#                 requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+#             else:
+#                 requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
+#             requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+#
+#     time.sleep(4)
+#     counter += 1
+
+
+
+
+
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
+API_TOKEN: str = 'BOT TOKEN HERE'
+
+# Создаем объекты бота и диспетчера
+bot: Bot = Bot(token=API_TOKEN)
+dp: Dispatcher = Dispatcher()
+
+
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
+
+# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer('Напиши мне что-нибудь и в ответ '
+                         'я пришлю тебе твое сообщение')
+
+
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" и "/help"
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
+
